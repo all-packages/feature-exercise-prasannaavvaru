717bc54dff22a1351f83d031d1efde7a0d257799
prasanna Kumar avvaru <phpdeveloper798@gmail.com>
2021-06-15 09:14:30 +0530

    super market test cases added

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
 <?php 
 
-$request = array(
-    array(
-        'item' => 'A',
-        'quantity' => 6,
-    ),
-    array(
-        'item' => 'A',
-        'quantity' => 9,
-    ),
-    array(
-        'item' => 'C',
-        'quantity' => 12,
-    ),
-    array(
-        'item' => 'D',
-        'quantity' => 10,
-    ),
-    array(
-        'item' => 'D',
-        'quantity' => 1,
-    )
-
-);
-
-
-
-echo "<pre>";
-print_r($request);
+require_once(__DIR__.'/vendor/autoload.php');
